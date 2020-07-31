b7e3b6476609c8eecbf8f925b67f1f878079accb
MireilleConscience <mireille.conscience@gmail.com>
2020-07-31 15:48:36 +0200

    agrego api user para el carrito, cambio de logo y algunas cosas de presentacion

---
+++ public/js/contenidoCarrito.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', function(){
+    const anclaje = document.getElementbyId('carrito');
+
+    for (const boton of buttonsHearts) {
+        boton.onclick = function() {
+            const anclaje = this;
+            const movieId = this.getAttribute('data-movieId');
+            
+            fetch('http://localhost:3000/api/users/addFavourite', {
+                method : 'POST',
+                body : JSON.stringify({
+                    user_id : 1,
+                    movie_id : movieId,
+                }),
+                headers : {
+                    'Content-type' : 'application/json'
+                }
+            })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (respuestaEnJson) {
+                //aqui la API me responda exitosamente
+                console.log(respuestaEnJson)
+
+                if (respuestaEnJson.status == 401) {
+                    alert(respuestaEnJson.response);
+                    return false;
+                }
+
+                alert(respuestaEnJson.response);
+
+                //busco a la etiqueta i 
+                const i = anclaje.querySelector('i');
+
+                //cambio el icono a lleno
+                i.classList.toggle('far');
+                i.classList.toggle('fas');
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+
+        }
+    }
+});
